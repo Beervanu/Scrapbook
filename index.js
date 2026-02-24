25b1103c252ef715d30bfb0c98f5bed6483a4d6e
Beervanu <fialhoj986@gmail.com>
2026-02-24 03:14:51 +0000

    buttons for mobile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let num_days = 1
 let params = new URLSearchParams(window.location.search)
 let day = parseInt(params.get("day"))
+let known_scroll_x = 0
 if (!day) 
 {
 	url = new URL(window.location.href)
@@ -19,22 +20,46 @@ fetch("photos.json").then(res => res.json()).then(photos=>
 		img_div.innerHTML += '<div class="photo_container" ><div class="border"><img class="photo" src="./Images/'+date +'/'+images[i]+'"></div></div>'
 	}
 	document.getElementById("Title").innerText = date+":"
-}
-)
+	
+	let left_button = document.getElementById("left")
+	let right_button = document.getElementById("right")
+	if (day ==1)
+	{
+		right_button.style.display="none"
+	}
+	else if (day==num_days)
+	{
+		left_button.style.display="none"
+	}
+	document.getElementById("left").addEventListener("click", (e)=> next_day(true))
+	document.getElementById("right").addEventListener("click", (e)=> next_day())
+})
 
+
+function next_day(reverse=false)
+{
+	url = new URL(window.location.href)
+	if (!reverse)
+	{
+		url.searchParams.set("day", Math.max(1,day-1))
+	}
+	else
+	{
+		url.searchParams.set("day", Math.min(num_days, day+1))
+	}
+	window.location.href = url
+}
 
 document.addEventListener("keydown", onArrowKey)
 function onArrowKey(e)
 {
-	url = new URL(window.location.href)
+	
 	if(e.key == "ArrowRight")
 	{
-		url.searchParams.set("day", Math.max(1,day-1))
-		window.location.href = url
+		next_day()
 	}
 	else if(e.key == "ArrowLeft")
 	{
-		url.searchParams.set("day", Math.min(num_days, day+1))
-		window.location.href = url
+		next_day(true)
 	}
 }
